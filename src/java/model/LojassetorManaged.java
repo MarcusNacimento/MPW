2f24ed4e17d9ad3e46541097d43dac368d91e4c7
MarcusVSN2022 <marcusvinicius2090@gmail.com>
2022-09-30 11:42:55 -0300

    telaslojas

---
+++ src/java/model/LojassetorManaged.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author labinf
+ */
+@ManagedBean
+public class LojassetorManaged {
+
+    public String doKabum() {
+
+        return "kabum";
+
+    }
+    
+    public String doPichau() {
+
+        return "pichau";
+
+    }
+    
+    public String doTerabyte() {
+
+        return "teratybe";
+
+    }
+
+    public LojassetorManaged() {
+    }
+
+}
